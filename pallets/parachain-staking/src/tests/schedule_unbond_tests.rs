92bd49d7eb643912c9594840e5b8e08ef694b1b9
Nahu Seyoum <nahu.seyoum@artos.io>
2022-11-20 01:32:44 +0000

    add event check

--- pallets/parachain-staking/src/tests/schedule_unbond_tests.rs
+++ pallets/parachain-staking/src/tests/schedule_unbond_tests.rs
@@ -3,12 +3,12 @@
 #![cfg(test)]
 
 use crate::{
-    encode_signed_schedule_nominator_unbond_params,
+    assert_event_emitted, encode_signed_schedule_nominator_unbond_params,
     mock::{
         build_proof, sign, AccountId, AvnProxy, Call as MockCall, ExtBuilder,
-        MinNominationPerCollator, Origin, ParachainStaking, Signature, Staker, Test, TestAccount,
+        MinNominationPerCollator, Origin, ParachainStaking, Signature, Staker, Test, TestAccount, System
     },
-    Config, Proof,
+    Config, Proof, Event, EraIndex
 };
 use frame_support::assert_ok;
 use frame_system::{self as system, RawOrigin};
@@ -100,6 +100,16 @@ mod proxy_signed_schedule_unbond {
         return build_proof(&staker.account_id, &staker.relayer, signature)
     }
 
+
+    fn unbond_event_emitted(nominator: AccountId) -> bool {
+        System::events()
+            .into_iter()
+            .map(|r| r.event)
+            .filter_map(|e| if let crate::mock::Event::ParachainStaking(inner) = e { Some(inner) } else { None })
+            .filter_map(|inner| if let Event::NominationDecreaseScheduled{ nominator, .. } = inner { Some(nominator) } else { None })
+            .any(|n| n == nominator)
+    }
+
     #[test]
     fn unbond_test() {
         ExtBuilder::default().build().execute_with(|| {
@@ -155,10 +165,7 @@ mod proxy_signed_schedule_unbond {
                 );
                 assert_ok!(AvnProxy::proxy(Origin::signed(staker.relayer), unbond_call, None));
 
-                // assert_ok!(ParachainStaking::signed_schedule_nominator_unbond(
-                //     Origin::signed(staker.account_id),
-                //     AMOUNT_TO_UNBOND.with(|v| *v.borrow())
-                // ));
+                assert!(unbond_event_emitted(Staker::default().account_id));
             });
     }
 }
