bb349ff2048d1d2395a253ce430260f493d3db64
Micael Franco <micael.franco@aventus.io>
2022-11-22 00:35:03 +0000

    avn test fixes

--- pallets/avn/src/tests/tests.rs
+++ pallets/avn/src/tests/tests.rs
@@ -3,10 +3,10 @@ use sp_runtime::testing::UintAuthorityId;
 
 #[test]
 fn test_is_primary_blocknumber_1() {
-    let mut ext = ExtBuilder::build_default().with_validators().as_externality();
+    let mut ext = ExtBuilder::build_default().with_validators(vec![1,2,3]).as_externality();
     ext.execute_with(|| {
         let block_number = 1;
-        let expected_primary = 2;
+        let expected_primary = TestAccount::derive_validator(2).account_id;
         let result = AVN::is_primary(block_number, &expected_primary);
         assert!(result.is_ok(), "Getting primary validator failed");
         assert_eq!(result.unwrap(), true);
@@ -15,10 +15,10 @@ fn test_is_primary_blocknumber_1() {
 
 #[test]
 fn test_is_primary_blocknumber_2() {
-    let mut ext = ExtBuilder::build_default().with_validators().as_externality();
+    let mut ext = ExtBuilder::build_default().with_validators(vec![1,2,3]).as_externality();
     ext.execute_with(|| {
         let block_number = 2;
-        let expected_primary = 3;
+        let expected_primary = TestAccount::derive_validator(3).account_id;
         let result = AVN::is_primary(block_number, &expected_primary);
         assert!(result.is_ok(), "Getting primary validator failed");
         assert_eq!(result.unwrap(), true);
@@ -27,10 +27,10 @@ fn test_is_primary_blocknumber_2() {
 
 #[test]
 fn test_is_primary_blocknumber_3() {
-    let mut ext = ExtBuilder::build_default().with_validators().as_externality();
+    let mut ext = ExtBuilder::build_default().with_validators(vec![1,2,3]).as_externality();
     ext.execute_with(|| {
         let block_number = 3;
-        let expected_primary = 1;
+        let expected_primary = TestAccount::derive_validator(1).account_id;
         let result = AVN::is_primary(block_number, &expected_primary);
         assert!(result.is_ok(), "Getting primary validator failed");
         assert_eq!(result.unwrap(), true);
@@ -39,10 +39,10 @@ fn test_is_primary_blocknumber_3() {
 
 #[test]
 fn test_is_primary_blocknumber_100() {
-    let mut ext = ExtBuilder::build_default().with_validators().as_externality();
+    let mut ext = ExtBuilder::build_default().with_validators(vec![1,2,3]).as_externality();
     ext.execute_with(|| {
         let block_number = 100;
-        let expected_primary = 2;
+        let expected_primary = TestAccount::derive_validator(2).account_id;
         let result = AVN::is_primary(block_number, &expected_primary);
         assert!(result.is_ok(), "Getting primary validator failed");
         assert_eq!(result.unwrap(), true);
@@ -54,7 +54,8 @@ fn is_primary_fails_with_no_validators() {
     let mut ext = ExtBuilder::build_default().as_externality();
     ext.execute_with(|| {
         let block_number = 1;
-        let result = AVN::is_primary(block_number, &1);
+        let dummy_validator = TestAccount::derive_validator(1).account_id;
+        let result = AVN::is_primary(block_number, &dummy_validator);
         assert!(result.is_err(), "Getting primary validator should have failed");
     });
 }
@@ -63,7 +64,7 @@ fn is_primary_fails_with_no_validators() {
 
 #[test]
 fn test_local_authority_keys_empty() {
-    let mut ext = ExtBuilder::build_default().with_validators().as_externality();
+    let mut ext = ExtBuilder::build_default().with_validators(vec![1,2,3]).as_externality();
     ext.execute_with(|| {
         let current_node_validator = AVN::get_validator_for_current_node();
         assert!(current_node_validator.is_none());
@@ -72,11 +73,11 @@ fn test_local_authority_keys_empty() {
 
 #[test]
 fn test_local_authority_keys_valid() {
-    let mut ext = ExtBuilder::build_default().with_validators().as_externality();
+    let mut ext = ExtBuilder::build_default().with_validators(vec![1,2,3]).as_externality();
     ext.execute_with(|| {
         UintAuthorityId::set_all_keys(vec![1, 2, 3]);
         let current_node_validator = AVN::get_validator_for_current_node().unwrap();
-        assert_eq!(current_node_validator.account_id, 1);
+        assert_eq!(current_node_validator.account_id, TestAccount::derive_account_id(1));
         assert_eq!(current_node_validator.key, UintAuthorityId(1));
     });
 }
