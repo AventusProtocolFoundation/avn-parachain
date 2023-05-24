61c0e6318a4f6fe2fda6596554efcd9b27788be6
nahuseyoum <nahu.seyoum@aventus.io>
2023-05-25 00:50:47 +0100

    SYS-3344 Add percentage based fee config

--- pallets/avn-transaction-payment/src/fee_adjustment_config.rs
+++ pallets/avn-transaction-payment/src/fee_adjustment_config.rs
@@ -1,12 +1,13 @@
 use crate::{BalanceOf, Config, Error};
 use codec::{Decode, Encode, MaxEncodedLen};
-use sp_runtime::{scale_info::TypeInfo, traits::Zero};
+use sp_runtime::{scale_info::TypeInfo, traits::Zero, Perbill, Saturating};
 use sp_std::{fmt::Debug};
 
 #[derive(Encode, Decode, MaxEncodedLen, Clone, PartialEq, Eq, TypeInfo)]
 #[scale_info(skip_type_params(T))]
 pub enum FeeAdjustmentConfig<T: Config> {
     FixedFee(FixedFeeConfig<T>),
+    PercentageFee(PercentageFeeConfig<T>),
     None,
 }
 
@@ -15,6 +16,9 @@ impl<T: Config> Debug for FeeAdjustmentConfig<T> {
         match self {
             Self::FixedFee(c) => {
                 write!(f, "Fixed fee[{:?}]", c.fee)
+            },
+            Self::PercentageFee(c) => {
+                write!(f, "Percentage fee[{}]", c.percentage)
             },
             Self::None => {
                 write!(f, "Fee config unknown")
@@ -33,6 +37,7 @@ impl<T: Config> FeeAdjustmentConfig<T> {
     pub fn is_valid(&self) -> bool {
         return match self {
             FeeAdjustmentConfig::FixedFee(c) => c.is_valid(),
+            FeeAdjustmentConfig::PercentageFee(c) => c.is_valid(),
             FeeAdjustmentConfig::None => false,
         }
     }
@@ -40,6 +45,7 @@ impl<T: Config> FeeAdjustmentConfig<T> {
     pub fn is_active(&self) -> bool {
         return match self {
             FeeAdjustmentConfig::FixedFee(_) => true,
+            FeeAdjustmentConfig::PercentageFee(_) => true,
             FeeAdjustmentConfig::None => false,
         }
     }
@@ -47,6 +53,7 @@ impl<T: Config> FeeAdjustmentConfig<T> {
     pub fn get_fee(&self, original_fee: BalanceOf<T>) -> Result<BalanceOf<T>, Error<T>> {
         return match self {
             FeeAdjustmentConfig::FixedFee(c) => c.get_fee(original_fee),
+            FeeAdjustmentConfig::PercentageFee(c) => c.get_fee(original_fee),
             FeeAdjustmentConfig::None => Ok(original_fee),
         }
     }
@@ -70,6 +77,33 @@ impl<T: Config> FixedFeeConfig<T> {
     pub fn get_fee(&self, original_fee: BalanceOf<T>) -> Result<BalanceOf<T>, Error<T>> {
         if self.is_active() {
             return Ok(self.fee)
+        }
+
+        // There is no adjutment to make so return the original fee
+        return Ok(original_fee)
+    }
+}
+
+#[derive(Encode, Decode, MaxEncodedLen, Default, Clone, PartialEq, Debug, Eq, TypeInfo, Copy)]
+#[scale_info(skip_type_params(T))]
+pub struct PercentageFeeConfig<T: Config> {
+    pub percentage: u32,
+    #[codec(skip)]
+    _marker: PhantomData<T>,
+}
+
+impl<T: Config> PercentageFeeConfig<T> {
+    pub fn is_valid(&self) -> bool {
+        return !self.percentage.is_zero()
+    }
+
+    pub fn is_active(&self) -> bool {
+        return true
+    }
+
+    pub fn get_fee(&self, original_fee: BalanceOf<T>) -> Result<BalanceOf<T>, Error<T>> {
+        if self.is_active() {
+            return Ok(Perbill::from_percent(self.percentage) * original_fee)
         }
 
         // There is no adjutment to make so return the original fee
