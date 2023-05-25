0679dfccd8a24b219acd816b37a520fba16b6576
nahuseyoum <nahu.seyoum@aventus.io>
2023-05-25 09:30:49 +0100

    SYS-3344 Implement Time based fee config

--- pallets/avn-transaction-payment/src/fee_adjustment_config.rs
+++ pallets/avn-transaction-payment/src/fee_adjustment_config.rs
@@ -3,15 +3,23 @@ use codec::{Decode, Encode, MaxEncodedLen};
 use sp_runtime::{scale_info::TypeInfo, traits::Zero, Perbill, Saturating};
 use sp_std::{fmt::Debug};
 
+#[derive(Encode, Decode, MaxEncodedLen, Clone, PartialEq, Eq, TypeInfo, Copy)]
+#[scale_info(skip_type_params(T))]
+pub enum FeeType<T: Config> {
+    FixedFee(FixedFeeConfig<T>),
+    PercentageFee(PercentageFeeConfig<T>),
+    None,
+}
 #[derive(Encode, Decode, MaxEncodedLen, Clone, PartialEq, Eq, TypeInfo)]
 #[scale_info(skip_type_params(T))]
 pub enum FeeAdjustmentConfig<T: Config> {
     FixedFee(FixedFeeConfig<T>),
     PercentageFee(PercentageFeeConfig<T>),
+    TimeBased(TimeBasedConfig<T>),
     None,
 }
 
-impl<T: Config> Debug for FeeAdjustmentConfig<T> {
+impl<T: Config> Debug for FeeType<T> {
     fn fmt(&self, f: &mut sp_std::fmt::Formatter<'_>) -> sp_std::fmt::Result {
         match self {
             Self::FixedFee(c) => {
@@ -21,12 +29,35 @@ impl<T: Config> Debug for FeeAdjustmentConfig<T> {
                 write!(f, "Percentage fee[{}]", c.percentage)
             },
             Self::None => {
+                write!(f, "Unknwon fee type")
+            },
+        }
+    }
+}
+impl<T: Config> Debug for FeeAdjustmentConfig<T> {
+    fn fmt(&self, f: &mut sp_std::fmt::Formatter<'_>) -> sp_std::fmt::Result {
+        match self {
+            Self::FixedFee(c) => {
+                write!(f, "Fixed fee[{:?}]", c.fee)
+            },
+            Self::PercentageFee(c) => {
+                write!(f, "Percentage fee[{}]", c.percentage)
+            },
+            Self::TimeBased(c) => {
+                write!(f, "Time based fee[{:?}, {:?}]", c.end_block_number, c.fee_type)
+            },
+            Self::None => {
                 write!(f, "Fee config unknown")
             },
         }
     }
 }
 
+impl<T: Config> Default for FeeType<T> {
+    fn default() -> Self {
+        FeeType::None
+    }
+}
 impl<T: Config> Default for FeeAdjustmentConfig<T> {
     fn default() -> Self {
         FeeAdjustmentConfig::None
@@ -38,6 +69,7 @@ impl<T: Config> FeeAdjustmentConfig<T> {
         return match self {
             FeeAdjustmentConfig::FixedFee(c) => c.is_valid(),
             FeeAdjustmentConfig::PercentageFee(c) => c.is_valid(),
+            FeeAdjustmentConfig::TimeBased(c) => c.is_valid(),
             FeeAdjustmentConfig::None => false,
         }
     }
@@ -46,6 +78,7 @@ impl<T: Config> FeeAdjustmentConfig<T> {
         return match self {
             FeeAdjustmentConfig::FixedFee(_) => true,
             FeeAdjustmentConfig::PercentageFee(_) => true,
+            FeeAdjustmentConfig::TimeBased(c) => c.is_active(),
             FeeAdjustmentConfig::None => false,
         }
     }
@@ -54,6 +87,7 @@ impl<T: Config> FeeAdjustmentConfig<T> {
         return match self {
             FeeAdjustmentConfig::FixedFee(c) => c.get_fee(original_fee),
             FeeAdjustmentConfig::PercentageFee(c) => c.get_fee(original_fee),
+            FeeAdjustmentConfig::TimeBased(c) => c.get_fee(original_fee),
             FeeAdjustmentConfig::None => Ok(original_fee),
         }
     }
@@ -111,3 +145,45 @@ impl<T: Config> PercentageFeeConfig<T> {
     }
 }
 
+#[derive(Encode, Decode, MaxEncodedLen, Default, Clone, PartialEq, Debug, Eq, TypeInfo)]
+#[scale_info(skip_type_params(T))]
+pub struct TimeBasedConfig<T: Config> {
+    pub fee_type: FeeType<T>,
+    end_block_number: T::BlockNumber,
+}
+
+impl<T: Config> TimeBasedConfig<T> {
+    pub fn is_valid(&self) -> bool {
+        return !self.end_block_number.is_zero() && self.fee_type != FeeType::None
+    }
+
+    pub fn is_active(&self) -> bool {
+        return self.end_block_number >= <frame_system::Pallet<T>>::block_number()
+    }
+
+    pub fn get_fee(&self, original_fee: BalanceOf<T>) -> Result<BalanceOf<T>, Error<T>> {
+        if self.is_active() {
+            return calculate_fee::<T>(original_fee, &self.fee_type)
+        }
+
+        // There is no adjutment to make so return the original fee
+        return Ok(original_fee)
+    }
+
+    pub fn new(fee_type: FeeType<T>, duration: T::BlockNumber) -> Self {
+        let end_block_number = <frame_system::Pallet<T>>::block_number().saturating_add(duration);
+        return TimeBasedConfig::<T> { fee_type, end_block_number }
+    }
+}
+
+
+fn calculate_fee<T: Config>(
+    original_fee: BalanceOf<T>,
+    fee_type: &FeeType<T>,
+) -> Result<BalanceOf<T>, Error<T>> {
+    return match fee_type {
+        FeeType::FixedFee(f) => Ok(f.fee),
+        FeeType::PercentageFee(p) => return Ok(Perbill::from_percent(p.percentage) * original_fee),
+        _ => Err(Error::InvalidFeeType),
+    }
+}
