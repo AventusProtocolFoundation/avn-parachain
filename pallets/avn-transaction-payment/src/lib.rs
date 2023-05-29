70617483d794785729aadab3d50f1e7303778e76
Nahu <39748285+nahuseyoum@users.noreply.github.com>
2023-05-29 11:02:43 +0100

    SYS-3346 New sudo extrinsic to add/update known sender (#144)

--- pallets/avn-transaction-payment/src/lib.rs
+++ pallets/avn-transaction-payment/src/lib.rs
@@ -17,7 +17,11 @@ pub use pallet::*;
 use sp_runtime::traits::Dispatchable;
 
 pub mod fee_adjustment_config;
-use fee_adjustment_config::*;
+use fee_adjustment_config::{
+    AdjustmentType::{TimeBased, TransactionBased},
+    *,
+};
+
 #[frame_support::pallet]
 pub mod pallet {
     use super::*;
@@ -47,11 +51,18 @@ pub mod pallet {
 
     #[pallet::event]
     #[pallet::generate_deposit(pub fn deposit_event)]
-    pub enum Event<T: Config> {}
+    pub enum Event<T: Config> {
+        /// A new known sender has been added
+        KnownSenderAdded { known_sender: T::AccountId, adjustment: FeeAdjustmentConfig<T> },
+        /// Adjustments have been updated for an existing known sender
+        KnownSenderUpdated { known_sender: T::AccountId, adjustment: FeeAdjustmentConfig<T> },
+    }
 
     #[pallet::error]
     pub enum Error<T> {
+        InvalidFeeConfig,
         InvalidFeeType,
+        KnownSenderMustMatchAccount,
     }
 
     #[pallet::storage]
@@ -61,7 +72,66 @@ pub mod pallet {
         StorageMap<_, Blake2_128Concat, T::AccountId, FeeAdjustmentConfig<T>, ValueQuery>;
 
     #[pallet::call]
-    impl<T: Config> Pallet<T> {}
+    impl<T: Config> Pallet<T> {
+        #[pallet::call_index(0)]
+        #[pallet::weight(0)]
+        pub fn set_known_sender(
+            origin: OriginFor<T>,
+            known_sender: T::AccountId,
+            config: AdjustmentInput<T>,
+        ) -> DispatchResult {
+            frame_system::ensure_root(origin)?;
+
+            let mut fee_adjustment_config: FeeAdjustmentConfig<T> = Default::default();
+            if config.adjustment_type != AdjustmentType::None {
+                match config.adjustment_type {
+                    TimeBased(b) => {
+                        fee_adjustment_config = FeeAdjustmentConfig::TimeBased(
+                            TimeBasedConfig::new(config.fee_type, b.duration),
+                        );
+                    },
+                    TransactionBased(i) => {
+                        fee_adjustment_config =
+                            FeeAdjustmentConfig::TransactionBased(TransactionBasedConfig::new(
+                                config.fee_type,
+                                known_sender.clone(),
+                                i.number_of_transactions,
+                            ));
+                    },
+                    _ => {},
+                }
+            } else {
+                match config.fee_type {
+                    FeeType::FixedFee(f) => {
+                        fee_adjustment_config = FeeAdjustmentConfig::FixedFee(f);
+                    },
+                    FeeType::PercentageFee(p) => {
+                        fee_adjustment_config = FeeAdjustmentConfig::PercentageFee(p);
+                    },
+                    _ => {},
+                }
+            }
+
+            ensure!(fee_adjustment_config.is_valid() == true, Error::<T>::InvalidFeeConfig);
+
+            let sender_exists = <KnownSenders<T>>::contains_key(&known_sender);
+            <KnownSenders<T>>::insert(&known_sender, &fee_adjustment_config);
+
+            if !sender_exists {
+                Self::deposit_event(Event::<T>::KnownSenderAdded {
+                    known_sender,
+                    adjustment: fee_adjustment_config,
+                });
+            } else {
+                Self::deposit_event(Event::<T>::KnownSenderUpdated {
+                    known_sender,
+                    adjustment: fee_adjustment_config,
+                });
+            }
+
+            Ok(())
+        }
+    }
 }
 
 pub(crate) type BalanceOf<T> =
