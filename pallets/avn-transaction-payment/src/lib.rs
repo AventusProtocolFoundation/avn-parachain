9329af243e2edd711229cfeea08062134c8f9316
micaelffrancoAV <97438428+micaelffrancoAV@users.noreply.github.com>
2023-06-02 16:00:25 +0100

    SYS-3348_set_known_sender_tests (#153)

--- pallets/avn-transaction-payment/src/lib.rs
+++ pallets/avn-transaction-payment/src/lib.rs
@@ -307,6 +307,10 @@ where
 #[path = "tests/mock.rs"]
 mod mock;
 
+#[cfg(test)]
+#[path = "tests/set_known_sender_tests.rs"]
+pub mod set_known_sender_tests;
+
 pub mod default_weights;
 pub use default_weights::WeightInfo;
 
---
+++ pallets/avn-transaction-payment/src/tests/set_known_sender_tests.rs
@@ -0,0 +1,238 @@
+use super::*;
+use crate::mock::{
+    new_test_ext, AccountId, AvnTransactionPayment, RuntimeOrigin, TestAccount, TestRuntime,
+};
+
+use frame_support::{assert_noop, assert_ok};
+
+fn to_acc_id(id: u64) -> AccountId {
+    return TestAccount::new(id).account_id()
+}
+
+mod set_known_senders {
+    use super::*;
+
+    mod succeeds_when {
+        use super::*;
+
+        #[test]
+        fn call_is_set_with_correct_percentage_fee() {
+            new_test_ext().execute_with(|| {
+                let account_1 = to_acc_id(1u64);
+
+                let config = AdjustmentInput::<TestRuntime> {
+                    fee_type: FeeType::PercentageFee(PercentageFeeConfig {
+                        percentage: 10,
+                        _marker: sp_std::marker::PhantomData::<TestRuntime>,
+                    }),
+                    adjustment_type: AdjustmentType::None,
+                };
+
+                assert_ok!(AvnTransactionPayment::set_known_sender(
+                    RuntimeOrigin::root(),
+                    account_1,
+                    config,
+                ));
+            })
+        }
+
+        #[test]
+        fn call_is_set_with_correct_fixed_fee() {
+            new_test_ext().execute_with(|| {
+                let account_1 = to_acc_id(1u64);
+
+                let config = AdjustmentInput::<TestRuntime> {
+                    fee_type: FeeType::FixedFee(FixedFeeConfig { fee: 10 }),
+                    adjustment_type: AdjustmentType::None,
+                };
+
+                assert_ok!(AvnTransactionPayment::set_known_sender(
+                    RuntimeOrigin::root(),
+                    account_1,
+                    config,
+                ));
+            })
+        }
+
+        #[test]
+        fn call_is_set_with_correct_transaction_based_adjustment_type() {
+            new_test_ext().execute_with(|| {
+                let account_1 = to_acc_id(1u64);
+
+                let config = AdjustmentInput::<TestRuntime> {
+                    fee_type: FeeType::FixedFee(FixedFeeConfig { fee: 1 }),
+                    adjustment_type: AdjustmentType::TransactionBased(NumberOfTransactions {
+                        number_of_transactions: 5,
+                    }),
+                };
+
+                assert_ok!(AvnTransactionPayment::set_known_sender(
+                    RuntimeOrigin::root(),
+                    account_1,
+                    config,
+                ));
+            })
+        }
+
+        #[test]
+        fn call_is_set_with_correct_time_based_adjustment_type() {
+            new_test_ext().execute_with(|| {
+                let account_1 = to_acc_id(1u64);
+
+                let config = AdjustmentInput::<TestRuntime> {
+                    fee_type: FeeType::FixedFee(FixedFeeConfig { fee: 1 }),
+                    adjustment_type: AdjustmentType::TimeBased(Duration { duration: 1 }),
+                };
+
+                assert_ok!(AvnTransactionPayment::set_known_sender(
+                    RuntimeOrigin::root(),
+                    account_1,
+                    config,
+                ));
+            })
+        }
+    }
+
+    mod fails_when {
+        use super::*;
+
+        #[test]
+        fn call_is_set_with_zero_percentage_fee() {
+            new_test_ext().execute_with(|| {
+                let account_1 = to_acc_id(1u64);
+
+                let config = AdjustmentInput::<TestRuntime> {
+                    fee_type: FeeType::PercentageFee(PercentageFeeConfig {
+                        percentage: 0,
+                        _marker: sp_std::marker::PhantomData::<TestRuntime>,
+                    }),
+                    adjustment_type: AdjustmentType::None,
+                };
+
+                assert_noop!(
+                    AvnTransactionPayment::set_known_sender(
+                        RuntimeOrigin::root(),
+                        account_1,
+                        config,
+                    ),
+                    Error::<TestRuntime>::InvalidFeeConfig
+                );
+            })
+        }
+
+        #[test]
+        fn call_is_set_with_zero_fixed_fee() {
+            new_test_ext().execute_with(|| {
+                let account_1 = to_acc_id(1u64);
+
+                let config = AdjustmentInput::<TestRuntime> {
+                    fee_type: FeeType::FixedFee(FixedFeeConfig { fee: 0 }),
+                    adjustment_type: AdjustmentType::None,
+                };
+
+                assert_noop!(
+                    AvnTransactionPayment::set_known_sender(
+                        RuntimeOrigin::root(),
+                        account_1,
+                        config,
+                    ),
+                    Error::<TestRuntime>::InvalidFeeConfig
+                );
+            })
+        }
+
+        #[test]
+        fn call_is_set_with_zero_number_of_transactions() {
+            new_test_ext().execute_with(|| {
+                let account_1 = to_acc_id(1u64);
+
+                let config = AdjustmentInput::<TestRuntime> {
+                    fee_type: FeeType::FixedFee(FixedFeeConfig { fee: 1 }),
+                    adjustment_type: AdjustmentType::TransactionBased(NumberOfTransactions {
+                        number_of_transactions: 0,
+                    }),
+                };
+
+                assert_noop!(
+                    AvnTransactionPayment::set_known_sender(
+                        RuntimeOrigin::root(),
+                        account_1,
+                        config,
+                    ),
+                    Error::<TestRuntime>::InvalidFeeConfig
+                );
+            })
+        }
+
+        #[test]
+        fn call_is_set_with_zero_duration() {
+            new_test_ext().execute_with(|| {
+                let account_1 = to_acc_id(1u64);
+
+                let config = AdjustmentInput::<TestRuntime> {
+                    fee_type: FeeType::FixedFee(FixedFeeConfig { fee: 1 }),
+                    adjustment_type: AdjustmentType::TimeBased(Duration { duration: 0 }),
+                };
+
+                assert_noop!(
+                    AvnTransactionPayment::set_known_sender(
+                        RuntimeOrigin::root(),
+                        account_1,
+                        config,
+                    ),
+                    Error::<TestRuntime>::InvalidFeeConfig
+                );
+            })
+        }
+    }
+}
+
+mod remove_known_senders {
+    use super::*;
+
+    mod succeeds_when {
+        use super::*;
+
+        #[test]
+        fn call_is_set_with_correct_information() {
+            new_test_ext().execute_with(|| {
+                let account_1 = to_acc_id(1u64);
+
+                let config = AdjustmentInput::<TestRuntime> {
+                    fee_type: FeeType::PercentageFee(PercentageFeeConfig {
+                        percentage: 10,
+                        _marker: sp_std::marker::PhantomData::<TestRuntime>,
+                    }),
+                    adjustment_type: AdjustmentType::None,
+                };
+
+                assert_ok!(AvnTransactionPayment::set_known_sender(
+                    RuntimeOrigin::root(),
+                    account_1,
+                    config,
+                ));
+
+                assert_ok!(AvnTransactionPayment::remove_known_sender(
+                    RuntimeOrigin::root(),
+                    account_1
+                ));
+            })
+        }
+    }
+
+    mod fails_when {
+        use super::*;
+
+        #[test]
+        fn call_is_set_with_an_unknown_sender() {
+            new_test_ext().execute_with(|| {
+                let account_1 = to_acc_id(1u64);
+
+                assert_noop!(
+                    AvnTransactionPayment::remove_known_sender(RuntimeOrigin::root(), account_1),
+                    Error::<TestRuntime>::KnownSenderMissing
+                );
+            })
+        }
+    }
+}
