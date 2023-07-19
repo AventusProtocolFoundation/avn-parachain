a590032259522213be433d8d85c54829ca08e7ae
Thanos <56822898+thadouk@users.noreply.github.com>
2023-07-19 10:29:40 +0100

    SYS-3338 Converts Royalties and Batches size in bounded data
    Converts the code to use bounded data for Royalties and Batches.
    Updates tests, adds new ones that test the boundaries and updates
    benchmarks code.
    Removes older migration code as all chains have been migrated and there
    is no value converting the old data struct.

--- pallets/nft-manager/src/benchmarking.rs
+++ pallets/nft-manager/src/benchmarking.rs
@@ -71,6 +71,10 @@ fn bounded_unique_external_ref() -> BoundedVec<u8, NftExternalRefBound> {
         .expect("Unique external reference bound was exceeded.")
 }
 
+pub fn bounded_royalties(royalties: Vec<Royalty>) -> BoundedVec<Royalty, NftRoyaltiesBound> {
+    BoundedVec::try_from(royalties.clone()).expect("Royalty bound was exceeded.")
+}
+
 fn get_relayer<T: Config>() -> T::AccountId {
     let relayer_account: H256 =
         H256(hex!("0000000000000000000000000000000000000000000000000000000000000001"));
@@ -348,12 +352,12 @@ struct CreateBatch<T: Config> {
 }
 
 impl<T: Config> CreateBatch<T> {
-    fn new(number_of_royalties: u32) -> Self {
+    fn new(batch_size: u32) -> Self {
         let relayer_account_id = get_relayer::<T>();
         let (creator_key_pair, creator_account_id) = get_user_account::<T>();
 
-        let total_supply = 100;
-        let royalties = Self::setup_royalties(number_of_royalties);
+        let total_supply = batch_size as u64;
+        let royalties = Self::setup_royalties(NftRoyaltiesBound::get());
         let t1_authority = H160(hex!("0000000000000000000000000000000000000001"));
         let nonce = 0u64;
 
@@ -406,7 +410,7 @@ impl<T: Config> CreateBatch<T> {
         create_batch::<T>(
             U256::zero(),
             batch_id,
-            self.royalties.clone(),
+            self.bounded_royalties(),
             self.total_supply,
             self.t1_authority,
             self.creator_account_id.clone(),
@@ -415,6 +419,10 @@ impl<T: Config> CreateBatch<T> {
         <BatchNonces<T>>::mutate(&self.creator_account_id, |n| *n += 1);
 
         return batch_id
+    }
+
+    pub fn bounded_royalties(&self) -> BoundedVec<Royalty, NftRoyaltiesBound> {
+        bounded_royalties(self.royalties.clone())
     }
 }
 
@@ -435,7 +443,7 @@ impl<T: Config> MintBatchNft<T> {
         let (nft_owner_key_pair, nft_owner_account_id) = get_user_account::<T>();
 
         // create batch and list
-        let batch: CreateBatch<T> = CreateBatch::new(1);
+        let batch: CreateBatch<T> = CreateBatch::new(T::BatchBound::get());
         let batch_id = batch.create_batch_for_setup();
         <BatchOpenForSale<T>>::insert(batch_id, NftSaleType::Fiat);
 
@@ -506,7 +514,7 @@ impl<T: Config> ListBatch<T> {
 
         let market = NftSaleType::Fiat;
 
-        let batch: CreateBatch<T> = CreateBatch::new(1);
+        let batch: CreateBatch<T> = CreateBatch::new(T::BatchBound::get());
         let batch_id = batch.create_batch_for_setup();
         let nonce = <BatchNonces<T>>::get(&creator_account_id);
 
@@ -551,7 +559,7 @@ impl<T: Config> EndBatchSale<T> {
 
         let market = NftSaleType::Fiat;
 
-        let batch: CreateBatch<T> = CreateBatch::new(1);
+        let batch: CreateBatch<T> = CreateBatch::new(T::BatchBound::get());
         let batch_id = batch.create_batch_for_setup();
 
         <BatchOpenForSale<T>>::insert(batch_id, market);
@@ -596,7 +604,7 @@ benchmarks! {
         );
         assert_eq!(true, NftInfos::<T>::contains_key(&mint_nft.info_id));
         assert_eq!(
-            NftInfo::new(mint_nft.info_id, mint_nft.royalties, mint_nft.t1_authority),
+            NftInfo::new(mint_nft.info_id, bounded_royalties(mint_nft.royalties.clone()), mint_nft.t1_authority),
             <NftInfos<T>>::get(&mint_nft.info_id).unwrap()
         );
         assert_eq!(true, <UsedExternalReferences<T>>::contains_key(&mint_nft.unique_external_ref));
@@ -631,7 +639,7 @@ benchmarks! {
         );
         assert_eq!(true, NftInfos::<T>::contains_key(&mint_nft.info_id));
         assert_eq!(
-            NftInfo::new(mint_nft.info_id, mint_nft.royalties, mint_nft.t1_authority),
+            NftInfo::new(mint_nft.info_id, bounded_royalties(mint_nft.royalties.clone()), mint_nft.t1_authority),
             <NftInfos<T>>::get(&mint_nft.info_id).unwrap()
         );
         assert_eq!(true, <UsedExternalReferences<T>>::contains_key(&mint_nft.unique_external_ref));
@@ -754,7 +762,7 @@ benchmarks! {
         );
         assert_eq!(true, NftInfos::<T>::contains_key(&mint_nft.info_id));
         assert_eq!(
-            NftInfo::new(mint_nft.info_id, mint_nft.royalties, mint_nft.t1_authority),
+            NftInfo::new(mint_nft.info_id, bounded_royalties(mint_nft.royalties.clone()), mint_nft.t1_authority),
             <NftInfos<T>>::get(&mint_nft.info_id).unwrap()
         );
         assert_eq!(true, <UsedExternalReferences<T>>::contains_key(&mint_nft.unique_external_ref));
@@ -822,7 +830,7 @@ benchmarks! {
     }
 
     proxy_signed_create_batch {
-        let r in 1 .. MAX_NUMBER_OF_ROYALTIES;
+        let r in 1 .. T::BatchBound::get();
         let batch: CreateBatch<T> = CreateBatch::new(r);
         let call: <T as Config>::RuntimeCall = batch.generate_signed_create_batch();
         let boxed_call: Box<<T as Config>::RuntimeCall> = Box::new(call);
