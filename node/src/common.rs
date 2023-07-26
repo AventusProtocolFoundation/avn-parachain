c13e085010d7709cedf1484857862ad766994955
Nahu <39748285+nahuseyoum@users.noreply.github.com>
2023-07-26 14:24:57 +0100

    SYS-3521 SYS-3522 Open source avn node - part 2 (#187)

---
+++ node/src/common.rs
@@ -0,0 +1,97 @@
+use node_primitives::{AccountId, Balance, Block as BlockT, Index};
+use polkadot_service::BlakeTwo256;
+use sp_consensus_aura::sr25519::AuthorityId as AuraId;
+
+use sc_cli::ChainSpec;
+
+cfg_if::cfg_if! {
+if #[cfg(feature = "test-native-runtime")] {
+	pub use avn_test_runtime::{Block, RuntimeApi};
+	pub use crate::common::TestParachainExecutor as ParachainExecutor;
+}
+else {
+	pub use avn_parachain_runtime::{Block, RuntimeApi};
+	pub use crate::common::AvnParachainExecutor as ParachainExecutor;
+}
+}
+
+/// A set of APIs that polkadot-like runtimes must implement.
+pub trait AvnRuntimeApiCollection:
+	sp_transaction_pool::runtime_api::TaggedTransactionQueue<BlockT>
+	+ sp_api::ApiExt<BlockT>
+	+ sp_block_builder::BlockBuilder<BlockT>
+	+ substrate_frame_rpc_system::AccountNonceApi<BlockT, AccountId, Index>
+	+ pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<BlockT, Balance>
+	+ sp_api::Metadata<BlockT>
+	+ sp_offchain::OffchainWorkerApi<BlockT>
+	+ sp_session::SessionKeys<BlockT>
+	+ cumulus_primitives_core::CollectCollationInfo<BlockT>
+	+ sp_consensus_aura::AuraApi<BlockT, AuraId>
+where
+	<Self as sp_api::ApiExt<BlockT>>::StateBackend: sp_api::StateBackend<BlakeTwo256>,
+{
+}
+
+impl<Api> AvnRuntimeApiCollection for Api
+where
+	Api: sp_transaction_pool::runtime_api::TaggedTransactionQueue<BlockT>
+		+ sp_api::ApiExt<BlockT>
+		+ sp_block_builder::BlockBuilder<BlockT>
+		+ substrate_frame_rpc_system::AccountNonceApi<BlockT, AccountId, Index>
+		+ pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<BlockT, Balance>
+		+ sp_api::Metadata<BlockT>
+		+ sp_offchain::OffchainWorkerApi<BlockT>
+		+ sp_session::SessionKeys<BlockT>
+		+ cumulus_primitives_core::CollectCollationInfo<BlockT>
+		+ sp_consensus_aura::AuraApi<BlockT, AuraId>,
+	<Self as sp_api::ApiExt<BlockT>>::StateBackend: sp_api::StateBackend<BlakeTwo256>,
+{
+}
+
+/// Native executor type.
+
+#[cfg(feature = "avn-native-runtime")]
+pub struct AvnParachainExecutor;
+
+#[cfg(feature = "avn-native-runtime")]
+impl sc_executor::NativeExecutionDispatch for AvnParachainExecutor {
+	type ExtendHostFunctions = frame_benchmarking::benchmarking::HostFunctions;
+
+	fn dispatch(method: &str, data: &[u8]) -> Option<Vec<u8>> {
+		avn_parachain_runtime::api::dispatch(method, data)
+	}
+
+	fn native_version() -> sc_executor::NativeVersion {
+		avn_parachain_runtime::native_version()
+	}
+}
+
+#[cfg(feature = "test-native-runtime")]
+pub struct TestParachainExecutor;
+
+#[cfg(feature = "test-native-runtime")]
+impl sc_executor::NativeExecutionDispatch for TestParachainExecutor {
+	type ExtendHostFunctions = frame_benchmarking::benchmarking::HostFunctions;
+
+	fn dispatch(method: &str, data: &[u8]) -> Option<Vec<u8>> {
+		avn_test_runtime::api::dispatch(method, data)
+	}
+
+	fn native_version() -> sc_executor::NativeVersion {
+		avn_test_runtime::native_version()
+	}
+}
+
+pub trait AvnRuntimeIdentity {
+	fn is_test_runtime(&self) -> bool;
+	fn is_production(&self) -> bool;
+}
+
+impl AvnRuntimeIdentity for Box<dyn ChainSpec> {
+	fn is_production(&self) -> bool {
+		self.id().starts_with("avn_rococo") || self.id().starts_with("avn_polkadot")
+	}
+	fn is_test_runtime(&self) -> bool {
+		self.id().starts_with("avn_garde")
+	}
+}
