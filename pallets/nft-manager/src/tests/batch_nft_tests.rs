dc530ebdfb90ab85c42cd8353132eedaa63d9ef1
Thanos <56822898+thadouk@users.noreply.github.com>
2023-07-29 07:46:12 +0100

    SYS-3338 Convert WeakBoundedVec to BoundedVec
    Revert the stopgap solution for the migration and the migration code itself.
    This should be included in a separate release from the original migration.

--- pallets/nft-manager/src/tests/batch_nft_tests.rs
+++ pallets/nft-manager/src/tests/batch_nft_tests.rs
@@ -563,7 +563,7 @@ impl MintBatchNftContext {
         <Nfts<TestRuntime>>::remove(&self.nft_id);
         <NftInfos<TestRuntime>>::remove(&self.nft_id);
         <UsedExternalReferences<TestRuntime>>::remove(
-            &WeakBoundedVec::try_from(self.unique_external_ref.clone())
+            &BoundedVec::try_from(self.unique_external_ref.clone())
                 .expect("Unique external reference bound was exceeded."),
         );
     }
@@ -650,7 +650,7 @@ mod signed_mint_batch_nft {
                 assert_eq!(
                     true,
                     <UsedExternalReferences<TestRuntime>>::contains_key(
-                        WeakBoundedVec::try_from(context.unique_external_ref)
+                        BoundedVec::try_from(context.unique_external_ref)
                             .expect("Unique external reference bound was exceeded.")
                     )
                 );
