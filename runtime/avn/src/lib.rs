bc970fd58d73f5e83ec1c964ba7b1642cf7465ef
aventus-ci-agent <ci-agent-bot@aventus.io>
2023-08-14 13:22:16 +0000

    cargo package, spec and impl versions increased

--- runtime/avn/src/lib.rs
+++ runtime/avn/src/lib.rs
@@ -188,7 +188,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("avn-parachain"),
     impl_name: create_runtime_str!("avn-parachain"),
     authoring_version: 1,
-    spec_version: 33,
+    spec_version: 34,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
