faad938e2db5606f9add8ce85945d2b3dc627de3
fluorostani <39763263+fluorostani@users.noreply.github.com>
2023-10-18 14:20:32 +0100

    Update lib.rs

--- pallets/avn/src/lib.rs
+++ pallets/avn/src/lib.rs
@@ -364,11 +364,7 @@ impl<T: Config> Pallet<T> {
             }
         });
     }
-
-    pub fn is_block_finalised(block_number: T::BlockNumber) -> bool {
-        return T::FinalisedBlockChecker::is_finalised(block_number)
-    }
-
+    
     pub fn get_external_service_port_number() -> String {
         let stored_value =
             StorageValueRef::persistent(EXTERNAL_SERVICE_PORT_NUMBER_KEY).get::<Vec<u8>>();
