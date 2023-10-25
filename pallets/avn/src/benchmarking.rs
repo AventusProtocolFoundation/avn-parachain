427aa05e83c8dc2f3cf47e1483d445ce05d72009
Michael Brozhko <michael.brozhko@aventus.io>
2023-10-25 11:08:29 +0100

    update benchmarks

--- pallets/avn/src/benchmarking.rs
+++ pallets/avn/src/benchmarking.rs
@@ -1,7 +1,7 @@
-//! # Ethereum events pallet
-// Copyright 2022 Aventus Systems (UK) Ltd.
+//! # avn pallet
+// Copyright 2023 Aventus Systems (UK) Ltd.
 
-//! ethereum-events pallet benchmarking.
+//! avn pallet benchmarking.
 
 #![cfg(feature = "runtime-benchmarks")]
 
@@ -10,12 +10,26 @@ use super::*;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite};
 use frame_system::{EventRecord, RawOrigin};
 
+fn assert_last_event<T: Config>(generic_event: <T as Config>::RuntimeEvent) {
+    let events = frame_system::Pallet::<T>::events();
+    let system_event: <T as frame_system::Config>::RuntimeEvent = generic_event.into();
+    // compare to the last event record
+    let EventRecord { event, .. } = &events[events.len().saturating_sub(1 as usize)];
+    assert_eq!(event, &system_event);
+}
+
 benchmarks! {
     set_bridge_contract {
         let contract_address = H160::from([1; 20]);
+        let old_contract = <AvnBridgeContractAddress<T>>::get();
     }: set_bridge_contract(RawOrigin::Root, contract_address.clone())
     verify {
         assert!(<AvnBridgeContractAddress<T>>::get() == contract_address);
+        assert_last_event::<T>(Event::AvnBridgeContractUpdated {
+            old_contract: old_contract,
+            new_contract: contract_address,
+            }.into()
+        );
     }
 }
 
