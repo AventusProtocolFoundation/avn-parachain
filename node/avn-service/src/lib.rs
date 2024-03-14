5b899f64befe9e62fcfe8c9c61b5b9df8c5d30f0
Thanos <56822898+thadouk@users.noreply.github.com>
2024-03-14 16:15:25 +0000

    SYS-3635: extends the voting mechanism
    Introduces the data structures and the voting mechanism for accepting
    events ranges from ethereum.
    
    Ranges that are too big will be broken down into smaller partitions,
    voted upon individually, and accepted as one.
    
    Follow up commit will do the validate unsigned and processing of the
    partitions.
    
    Jira tickets:
    - SYS-3635
    - SYS-3930

--- node/avn-service/src/lib.rs
+++ node/avn-service/src/lib.rs
@@ -20,8 +20,8 @@ use secp256k1::{Secp256k1, SecretKey};
 use tide::{http::StatusCode, Error as TideError};
 pub use web3Secp256k1::SecretKey as web3SecretKey;
 
-pub mod extrinsic_utils;
 pub mod ethereum_events_handler;
+pub mod extrinsic_utils;
 pub mod keystore_utils;
 pub mod merkle_tree_utils;
 pub mod summary_utils;
