54fc4da7122ad51022669e583ecee978aeff1aad
Thanos Doukoudakis <56822898+thadouk@users.noreply.github.com>
2024-04-09 09:18:21 +0100

    SYS-3930: Implement Vote Processing (#371)
    This PR introduces support for processing votes upon achieving consensus. It extends extrinsics and data structures accordingly.
    
    Once the final vote is cast, the voting round for the Ethereum range partition is finalized, and the partition is processed.
    
    Additionally, this PR extends the subscription mechanism of the eth-bridge pallet. By default, pallets ignore processed events, requiring the implementation of a handler to process them.
    Benchmarking for certain actions will be addressed in a separate PR.
    
    Jira Tasks:
    - SYS-3930
    - SYS-3635

--- pallets/eth-bridge/src/types.rs
+++ pallets/eth-bridge/src/types.rs
@@ -1,4 +1,7 @@
 use crate::*;
+use sp_avn_common::event_types::ValidEvents;
+type EventsTypesLimit = ConstU32<20>;
+type EthBridgeEventsFilter = BoundedBTreeSet<ValidEvents, EventsTypesLimit>;
 
 // The different types of request this pallet can handle.
 #[derive(Encode, Decode, Clone, PartialEq, Eq, Debug, TypeInfo, MaxEncodedLen)]
@@ -124,4 +127,17 @@ pub struct ActiveEthTransaction<T: Config> {
     pub valid_tx_hash_corroborations: BoundedVec<T::AccountId, ConfirmationsLimit>,
     pub invalid_tx_hash_corroborations: BoundedVec<T::AccountId, ConfirmationsLimit>,
     pub tx_succeeded: bool,
+}
+
+#[derive(Encode, Decode, Clone, PartialEq, Eq, Debug, Default, TypeInfo, MaxEncodedLen)]
+pub struct ActiveEthRange {
+    pub range: EthBlockRange,
+    pub partition: u16,
+    pub event_types_filter: EthBridgeEventsFilter,
+}
+
+impl ActiveEthRange {
+    pub fn is_initial_range(&self) -> bool {
+        *self == Default::default()
+    }
 }
