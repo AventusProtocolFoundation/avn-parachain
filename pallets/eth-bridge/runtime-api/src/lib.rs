3c8594f3203f8214ba2cc80a6a9ac9d6ea62df96
IvanCholakov <ivan.cholakov@aventus.io>
2024-04-10 14:07:14 +0300

    feat: added runtime-api for eth-event polling

---
+++ pallets/eth-bridge/runtime-api/src/lib.rs
@@ -0,0 +1,23 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+use codec::Codec;
+use frame_support::dispatch::Vec;
+use sp_avn_common::event_discovery::{EthBlockRange, EthereumEventsPartition};
+use sp_core::{H160, H256};
+
+sp_api::decl_runtime_apis! {
+    pub trait EthEventHandlerApi<AccountId>
+            where
+        AccountId: Codec,
+    {
+        fn query_active_block_range()-> (EthBlockRange, u16);
+        fn query_has_author_casted_event_vote(account_id: AccountId) -> bool;
+        fn query_signatures() -> Vec<H256>;
+        fn query_bridge_contract() -> H160;
+        fn create_proof(account_id:AccountId, events_partition:EthereumEventsPartition)-> Vec<u8>;
+        fn submit_vote(
+            author: AccountId,
+            events_partition: EthereumEventsPartition,
+            signature: sp_core::sr25519::Signature
+        ) -> Result<(), ()>;
+    }
+}
