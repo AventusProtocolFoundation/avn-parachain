ae896c2519b55107a6d72eebcf388817f66e4841
Michael <62653655+MBrozhko34@users.noreply.github.com>
2024-05-22 11:00:28 +0100

    SYS-3965: added first stage migration of processed events mechanism to eth-bridge (#390)
    Co-authored-by: Thanos <56822898+thadouk@users.noreply.github.com>
    Co-authored-by: nahuseyoum <nahu.seyoum@aventus.io>

--- pallets/eth-bridge/src/tests/mock.rs
+++ pallets/eth-bridge/src/tests/mock.rs
@@ -6,17 +6,18 @@ use frame_system as system;
 use pallet_avn::{testing::U64To32BytesConverter, EthereumPublicKeyChecker};
 use pallet_session as session;
 use parking_lot::RwLock;
+use sp_avn_common::event_types::{EthEvent, EthEventId, LiftedData, ValidEvents};
 use sp_core::{
     offchain::{
         testing::{OffchainState, PoolState, TestOffchainExt, TestTransactionPoolExt},
         OffchainDbExt, OffchainWorkerExt, TransactionPoolExt,
     },
-    ConstU32, ConstU64, H256,
+    ConstU32, ConstU64, H256, U256,
 };
 use sp_runtime::{
     testing::{TestSignature, TestXt, UintAuthorityId},
     traits::{BlakeTwo256, ConvertInto, IdentityLookup},
-    BuildStorage, Perbill,
+    BuildStorage, DispatchError, DispatchResult, Perbill,
 };
 use sp_staking::offence::OffenceError;
 use std::{cell::RefCell, convert::From, sync::Arc};
@@ -47,10 +48,17 @@ impl ReportOffence<AccountId, IdentificationTuple, Offence> for OffenceHandler {
 pub struct Context {
     pub eth_tx_hash: H256,
     pub already_set_eth_tx_hash: H256,
+    pub mock_event_partition: EthereumEventsPartition,
+    pub bad_mock_event_partition: EthereumEventsPartition,
+    pub second_mock_event_partition: EthereumEventsPartition,
     pub test_signature: TestSignature,
+    pub test_signature_two: TestSignature,
+    pub test_signature_three: TestSignature,
     pub confirmation_signature: ecdsa::Signature,
     pub tx_succeeded: bool,
     pub author: Author<TestRuntime>,
+    pub author_two: Author<TestRuntime>,
+    pub author_three: Author<TestRuntime>,
     pub confirming_author: Author<TestRuntime>,
     pub second_confirming_author: Author<TestRuntime>,
     pub third_confirming_author: Author<TestRuntime>,
@@ -62,6 +70,8 @@ pub struct Context {
     pub lower_id: u32,
     pub block_number: BlockNumber,
     pub expected_lower_msg_hash: String,
+    pub eth_event_id: EthEventId,
+    pub bad_eth_event_id: EthEventId,
 }
 
 const ROOT_HASH: &str = "30b83f0d722d1d4308ab4660a72dbaf0a7392d5674eca3cd21d57256d42df7a0";
@@ -100,6 +110,7 @@ impl Config for TestRuntime {
     type AccountToBytesConvert = U64To32BytesConverter;
     type BridgeInterfaceNotification = TestRuntime;
     type ReportCorroborationOffence = OffenceHandler;
+    type ProcessedEventsChecker = Self;
     type EthereumEventsFilter = ();
 }
 
@@ -171,6 +182,19 @@ pub fn create_confirming_author(author_id: u64) -> Author<TestRuntime> {
     Author::<TestRuntime> { key: UintAuthorityId(author_id), account_id: author_id }
 }
 
+pub fn create_mock_event_partition(events: EthEvent, part: u16) -> EthereumEventsPartition {
+    let mut partition: BoundedBTreeSet<DiscoveredEvent, EventsBatchLimit> = BoundedBTreeSet::new();
+    partition
+        .try_insert(DiscoveredEvent { event: events.clone(), block: 2 })
+        .unwrap();
+    EthereumEventsPartition::new(
+        EthBlockRange { start_block: 1, length: 1000 },
+        part,
+        false,
+        partition,
+    )
+}
+
 pub fn lower_is_ready_to_be_claimed(lower_id: &u32) -> bool {
     LOWERSREADYTOCLAIM.with(|lowers| lowers.borrow_mut().iter().any(|l| l == lower_id))
 }
@@ -185,6 +209,8 @@ pub fn setup_context() -> Context {
         key: UintAuthorityId(primary_validator_id),
         account_id: primary_validator_id,
     };
+    let author_two = Author::<TestRuntime> { key: UintAuthorityId(22), account_id: 22 };
+    let author_three = Author::<TestRuntime> { key: UintAuthorityId(23), account_id: 23 };
     let mut confirming_validator_id: u64 = 1;
     if primary_validator_id == confirming_validator_id {
         confirming_validator_id += 1
@@ -194,20 +220,72 @@ pub fn setup_context() -> Context {
     let third_confirming_author = create_confirming_author(confirming_validator_id + 2);
     let fourth_confirming_author = create_confirming_author(confirming_validator_id + 3);
     let test_signature = generate_signature(author.clone(), b"test context");
+    let test_signature_two = generate_signature(author.clone(), b"test context");
+    let test_signature_three = generate_signature(author.clone(), b"test context");
     let tx_succeeded = false;
     let eth_tx_hash = H256::from_slice(&[0u8; 32]);
     let already_set_eth_tx_hash = H256::from_slice(&[1u8; 32]);
     let confirmation_signature = ecdsa::Signature::try_from(&[1; 65][0..65]).unwrap();
     let finalised_block_vec = Some(hex::encode(10u32.encode()).into());
+    let eth_event_id =
+        EthEventId { signature: ValidEvents::Lifted.signature(), transaction_hash: eth_tx_hash };
+    let bad_eth_event_id = EthEventId {
+        signature: ValidEvents::Lifted.signature(),
+        transaction_hash: H256::from_slice(&[6u8; 32]),
+    };
+    let bad_eth_event = EthEvent {
+        event_id: bad_eth_event_id.clone(),
+        event_data: sp_avn_common::event_types::EventData::LogLifted(LiftedData {
+            token_contract: H160::zero(),
+            sender_address: H160::zero(),
+            receiver_address: H256::zero(),
+            amount: 1,
+            nonce: U256::zero(),
+        }),
+    };
+    let mock_event_partition = create_mock_event_partition(
+        EthEvent {
+            event_id: eth_event_id.clone(),
+            event_data: sp_avn_common::event_types::EventData::LogLifted(LiftedData {
+                token_contract: H160::zero(),
+                sender_address: H160::zero(),
+                receiver_address: H256::zero(),
+                amount: 1,
+                nonce: U256::zero(),
+            }),
+        },
+        0,
+    );
+    let bad_mock_event_partition = create_mock_event_partition(bad_eth_event, 0);
+
+    let second_mock_event_partition = create_mock_event_partition(
+        EthEvent {
+            event_id: eth_event_id.clone(),
+            event_data: sp_avn_common::event_types::EventData::LogLifted(LiftedData {
+                token_contract: H160::zero(),
+                sender_address: H160::zero(),
+                receiver_address: H256::zero(),
+                amount: 1,
+                nonce: U256::zero(),
+            }),
+        },
+        1,
+    );
 
     UintAuthorityId::set_all_keys(vec![UintAuthorityId(primary_validator_id)]);
 
     Context {
         eth_tx_hash,
+        mock_event_partition,
+        second_mock_event_partition,
         already_set_eth_tx_hash,
         test_signature,
+        test_signature_two,
+        test_signature_three,
         tx_succeeded,
         author: author.clone(),
+        author_two: author_two.clone(),
+        author_three: author_three.clone(),
         confirming_author: confirming_author.clone(),
         second_confirming_author: second_confirming_author.clone(),
         third_confirming_author: third_confirming_author.clone(),
@@ -222,6 +300,9 @@ pub fn setup_context() -> Context {
         // if request_params changes, this should also change
         expected_lower_msg_hash: "5892dee772ffe3d97e9525b62805bbcd91bac29026536cfa09269623128280ca"
             .to_string(),
+        eth_event_id,
+        bad_mock_event_partition,
+        bad_eth_event_id,
     }
 }
 
@@ -402,5 +483,31 @@ impl BridgeInterfaceNotification for TestRuntime {
         }
 
         Ok(())
+    }
+
+    fn on_incoming_event_processed(event: &EthEvent) -> DispatchResult {
+        if event.event_id.transaction_hash == H256::from_slice(&[6u8; 32]) {
+            return Err(DispatchError::Other("Test - Bad event"))
+        }
+
+        Ok(())
+    }
+}
+
+thread_local! {
+    static PROCESSED_EVENTS: RefCell<Vec<(EthEventId, bool)>> = RefCell::new(vec![]);
+}
+
+pub fn insert_to_mock_processed_events(event_id: &EthEventId, processed: bool) {
+    PROCESSED_EVENTS.with(|l| l.borrow_mut().push((event_id.clone(), processed)));
+}
+
+impl ProcessedEventsChecker for TestRuntime {
+    fn processed_event_exists(event_id: &EthEventId) -> bool {
+        PROCESSED_EVENTS.with(|l| l.borrow().iter().any(|(event, _processed)| event == event_id))
+    }
+
+    fn add_processed_event(event_id: &EthEventId, accepted: bool) {
+        insert_to_mock_processed_events(event_id, accepted);
     }
 }
