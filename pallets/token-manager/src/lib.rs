f2a0f0324d276bcff6c985cd8dd529a773169279
Thanos Doukoudakis <56822898+thadouk@users.noreply.github.com>
2024-06-04 18:18:43 +0100

    SYS-4072 fix handling of eth-bridge processed events (#410)
    Adds handlers in Token manager and wire up for Runtime to process the first type of events from eth-bridge.
    
    Jira ticket:
    - SYS-4072

--- pallets/token-manager/src/lib.rs
+++ pallets/token-manager/src/lib.rs
@@ -988,10 +988,8 @@ impl<T: Config> Pallet<T> {
 
         Ok(())
     }
-}
 
-impl<T: Config> ProcessedEventHandler for Pallet<T> {
-    fn on_event_processed(event: &EthEvent) -> DispatchResult {
+    fn processed_event_handler(event: &EthEvent) -> DispatchResult {
         return match &event.event_data {
             EventData::LogLifted(d) => return Self::process_lift(event, d),
             EventData::LogAvtGrowthLifted(d) => return Self::process_avt_growth_lift(event, d),
@@ -1000,6 +998,12 @@ impl<T: Config> ProcessedEventHandler for Pallet<T> {
             // Event handled or it is not for us, in which case ignore it.
             _ => Ok(()),
         }
+    }
+}
+
+impl<T: Config> ProcessedEventHandler for Pallet<T> {
+    fn on_event_processed(event: &EthEvent) -> DispatchResult {
+        Self::processed_event_handler(event)
     }
 }
 
@@ -1091,5 +1095,9 @@ impl<T: Config> BridgeInterfaceNotification for Pallet<T> {
         }
 
         Ok(())
+    }
+
+    fn on_incoming_event_processed(event: &EthEvent) -> DispatchResult {
+        Self::processed_event_handler(event)
     }
 }
