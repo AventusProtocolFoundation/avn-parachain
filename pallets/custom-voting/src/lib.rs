24aa68ed2472fb5ed74c7367c1546cc069d35bce
IvanCholakov <ivan.cholakov@aventus.io>
2024-06-24 11:50:34 +0300

    fix: added ethereum-vote extrinsic

--- pallets/custom-voting/src/lib.rs
+++ pallets/custom-voting/src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use frame_support::traits::{Polling, Currency};
+use frame_support::traits::{Currency, Polling};
 pub use pallet::*;
 pub use pallet_conviction_voting::{Config as VotingConfig, TallyOf};
 pub mod default_weights;
@@ -14,29 +14,56 @@ pub type BalanceOf<T, I = ()> =
 
 #[frame_support::pallet]
 pub mod pallet {
+    use crate::{default_weights::WeightInfo, BalanceOf, PollIndexOf, VotingConfig};
     use frame_support::{
         pallet_prelude::*,
-        traits::Polling,
+        traits::{Polling, Time},
     };
     use frame_system::pallet_prelude::*;
     use pallet_conviction_voting::AccountVote;
-    use crate::default_weights::WeightInfo;
-    use crate::{PollIndexOf, BalanceOf, VotingConfig};
-    use sp_runtime::ArithmeticError;
+    use sp_runtime::{traits::AtLeast32Bit, ArithmeticError};
+    use std::fmt::Debug;
+    use parity_scale_codec::{Encode, Decode};
 
-    #[pallet::config]
-    pub trait Config: frame_system::Config + VotingConfig {
-        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-        // type Balance: Parameter + From<u64> + Into<u128> + Copy;
-        type WeightInfo: WeightInfo;
+    #[derive(Encode, Decode, Clone, PartialEq, Eq, Debug, TypeInfo)]
+    #[scale_info(skip_type_params(T))]
+    pub struct VoteProof<T: Config> {
+        voter: T::AccountId,
+        vote: AccountVote<BalanceOf<T>>,
+        timestamp: T::Moment,
+        ethereum_signature: [u8; 65],
     }
-
+    #[pallet::config]
+    pub trait Config: frame_system::Config + VotingConfig
+    where
+        <Self as frame_system::Config>::AccountId: Debug,
+    {
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+        type WeightInfo: WeightInfo;
+        type TimeProvider: Time<Moment = Self::Moment>;
+        type MaxVoteAge: Get<Self::Moment>;
+        type Moment: Clone
+            + Copy
+            + PartialOrd
+            + AtLeast32Bit
+            + Default
+            + From<u64>
+            + Debug
+            + TypeInfo;
+    }
     #[pallet::pallet]
     pub struct Pallet<T>(_);
+
+    #[pallet::storage]
+    #[pallet::getter(fn processed_votes)]
+    pub type ProcessedVotes<T: Config> =
+        StorageMap<_, Blake2_128Concat, T::AccountId, bool, ValueQuery>;
 
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
+        VoteRecorded(T::AccountId, PollIndexOf<T>),
+        EthereumVoteProcessed(T::AccountId, PollIndexOf<T>),
     }
 
     #[pallet::error]
@@ -46,6 +73,9 @@ pub mod pallet {
         MaxVotesReached,
         AlreadyDelegating,
         NotOngoing,
+        AlreadyVoted,
+        FutureTimestamp,
+        VoteTooOld,
     }
 
     #[pallet::call]
@@ -59,34 +89,83 @@ pub mod pallet {
         ) -> DispatchResult {
             let who = ensure_signed(origin.clone())?;
 
+            Self::do_vote(who, poll_index, vote)
+        }
+
+        #[pallet::call_index(1)]
+        #[pallet::weight(<T as Config>::WeightInfo::custom_vote_weight())]
+        pub fn submit_ethereum_vote(
+            origin: OriginFor<T>,
+            poll_index: PollIndexOf<T>,
+            vote_proof: VoteProof<T>,
+        ) -> DispatchResult {
+            let _ = ensure_signed(origin)?;
+
+            ensure!(
+                !ProcessedVotes::<T>::contains_key(&vote_proof.voter),
+                Error::<T>::AlreadyVoted
+            );
+
+            let now = T::TimeProvider::now();
+            ensure!(vote_proof.timestamp <= now, Error::<T>::FutureTimestamp);
+            ensure!(now - vote_proof.timestamp <= T::MaxVoteAge::get(), Error::<T>::VoteTooOld);
+
+            ProcessedVotes::<T>::insert(&vote_proof.voter, true);
+
+            Self::do_vote(vote_proof.voter.clone(), poll_index, vote_proof.vote)?;
+
+            Self::deposit_event(Event::EthereumVoteProcessed(vote_proof.voter, poll_index));
+
+            Ok(())
+        }
+    }
+
+    impl<T: Config> Pallet<T> {
+        fn do_vote(
+            who: T::AccountId,
+            poll_index: PollIndexOf<T>,
+            vote: AccountVote<BalanceOf<T>>,
+        ) -> DispatchResult {
             <T as VotingConfig>::Polls::try_access_poll(poll_index, |poll_status| {
                 let (tally, class) = poll_status.ensure_ongoing().ok_or(Error::<T>::NotOngoing)?;
-                pallet_conviction_voting::VotingFor::<T, ()>::try_mutate(who, &class, |voting| {
-                    if let pallet_conviction_voting::Voting::Casting(pallet_conviction_voting::Casting { ref mut votes, delegations, .. }) = voting {
-                        match votes.binary_search_by_key(&poll_index, |i| i.0) {
-                            Ok(i) => {
-                                tally.remove(votes[i].1).ok_or(ArithmeticError::Underflow)?;
-                                if let Some(approve) = votes[i].1.as_standard() {
-                                    tally.reduce(approve, *delegations);
-                                }
-                                votes[i].1 = vote;
+                pallet_conviction_voting::VotingFor::<T, ()>::try_mutate(
+                    who.clone(),
+                    &class,
+                    |voting| {
+                        if let pallet_conviction_voting::Voting::Casting(
+                            pallet_conviction_voting::Casting {
+                                ref mut votes, delegations, ..
                             },
-                            Err(i) => {
-                                votes
-                                    .try_insert(i, (poll_index, vote))
-                                    .map_err(|_| Error::<T>::MaxVotesReached)?;
-                            },
+                        ) = voting
+                        {
+                            match votes.binary_search_by_key(&poll_index, |i| i.0) {
+                                Ok(i) => {
+                                    tally.remove(votes[i].1).ok_or(ArithmeticError::Underflow)?;
+                                    if let Some(approve) = votes[i].1.as_standard() {
+                                        tally.reduce(approve, *delegations);
+                                    }
+                                    votes[i].1 = vote;
+                                },
+                                Err(i) => {
+                                    votes
+                                        .try_insert(i, (poll_index, vote))
+                                        .map_err(|_| Error::<T>::MaxVotesReached)?;
+                                },
+                            }
+                            tally.add(vote).ok_or(ArithmeticError::Overflow)?;
+                            if let Some(approve) = vote.as_standard() {
+                                tally.increase(approve, *delegations);
+                            }
+                        } else {
+                            return Err(Error::<T>::AlreadyDelegating.into())
                         }
-                        tally.add(vote).ok_or(ArithmeticError::Overflow)?;
-                        if let Some(approve) = vote.as_standard() {
-                            tally.increase(approve, *delegations);
-                        }
-                    } else {
-                        return Err(Error::<T>::AlreadyDelegating.into());
-                    }
-                    Ok(())
-                })
-            })
+                        Ok(())
+                    },
+                )
+            })?;
+
+            Self::deposit_event(Event::VoteRecorded(who, poll_index));
+            Ok(())
         }
     }
 }
