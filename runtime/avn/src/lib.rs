64cbca147ceeb03d9cff56d1dc259b0019fe08f0
Micael Franco <micael.franco@aventus.io>
2024-07-23 10:19:50 +0100

    SYS-4116: Enable lift on eth-bridge

--- runtime/avn/src/lib.rs
+++ runtime/avn/src/lib.rs
@@ -642,7 +642,7 @@ pub struct EthBridgeAvnRuntimeEventsFilter;
 impl EthereumEventsFilterTrait for EthBridgeAvnRuntimeEventsFilter {
     fn get_filter() -> EthBridgeEventsFilter {
         let allowed_events: BTreeSet<ValidEvents> =
-            vec![ValidEvents::AvtLowerClaimed].into_iter().collect();
+            vec![ValidEvents::AvtLowerClaimed, ValidEvents::Lifted].into_iter().collect();
 
         EthBridgeEventsFilter::try_from(allowed_events).unwrap_or_default()
     }
