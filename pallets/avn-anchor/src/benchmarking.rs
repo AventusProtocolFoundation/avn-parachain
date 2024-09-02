151f7ceaeb87c6f30310bf30c9140baaa007aff4
Ivan Cholakov <icholakov1@gmail.com>
2024-09-02 19:22:24 +0300

    Sys 4193 submit checksum with identity (#431)

--- pallets/avn-anchor/src/benchmarking.rs
+++ pallets/avn-anchor/src/benchmarking.rs
@@ -1,11 +1,10 @@
 #![cfg(feature = "runtime-benchmarks")]
 
 use super::*;
-
-use crate::{Pallet as AvnAnchor, *};
 use frame_benchmarking::{account, benchmarks, impl_benchmark_test_suite};
-use frame_system::{EventRecord, RawOrigin};
-use sp_runtime::traits::Bounded;
+use frame_system::RawOrigin;
+use frame_support::BoundedVec;
+use sp_std::prelude::*;
 
 const SEED: u32 = 0;
 
@@ -25,7 +24,6 @@ benchmarks! {
         let new_handler: T::AccountId = account("new_handler", 1, SEED);
         let name: BoundedVec<u8, ConstU32<32>> = BoundedVec::try_from(vec![0u8; 32]).unwrap();
 
-        // Register the initial handler
         Pallet::<T>::register_chain_handler(RawOrigin::Signed(old_handler.clone()).into(), name.clone())?;
     }: _(RawOrigin::Signed(old_handler.clone()), new_handler.clone())
     verify {
@@ -34,6 +32,21 @@ benchmarks! {
         let chain_data = ChainHandlers::<T>::get(&new_handler).unwrap();
         assert_eq!(chain_data.name, name);
     }
+
+    submit_checkpoint_with_identity {
+        let handler: T::AccountId = account("handler", 0, SEED);
+        let name: BoundedVec<u8, ConstU32<32>> = BoundedVec::try_from(vec![0u8; 32]).unwrap();
+        let checkpoint = H256::random();
+
+        Pallet::<T>::register_chain_handler(RawOrigin::Signed(handler.clone()).into(), name.clone())?;
+
+        let initial_checkpoint_id = NextCheckpointId::<T>::get();
+    }: _(RawOrigin::Signed(handler.clone()), checkpoint)
+    verify {
+        let chain_data = ChainHandlers::<T>::get(&handler).unwrap();
+        assert_eq!(Checkpoints::<T>::get(chain_data.chain_id, initial_checkpoint_id), checkpoint);
+        assert_eq!(NextCheckpointId::<T>::get(), initial_checkpoint_id + 1);
+    }
 }
 
-impl_benchmark_test_suite!(Pallet, crate::mock::new_test_ext(), crate::mock::TestRuntime,);
+impl_benchmark_test_suite!(Pallet, crate::mock::new_test_ext(), crate::mock::TestRuntime);
