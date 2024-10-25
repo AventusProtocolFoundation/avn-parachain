ad01444f29802147076765f3c1b5be25acaf5508
Ivan Cholakov <icholakov1@gmail.com>
2024-10-25 16:12:00 +0300

    Update runtime/avn/src/lib.rs
    Co-authored-by: Nahu <39748285+nahuseyoum@users.noreply.github.com>
    Signed-off-by: Ivan Cholakov <icholakov1@gmail.com>

--- runtime/avn/src/lib.rs
+++ runtime/avn/src/lib.rs
@@ -683,7 +683,7 @@ parameter_types! {
     pub const StringLimit: u32 = 50;
     pub const MetadataDepositBase: Balance = 1 * MILLI_AVT;
     pub const MetadataDepositPerByte: Balance = 100 * MICRO_AVT;
-    pub const DefaultCheckpointFee: Balance = 1 * AVT;
+    pub const DefaultCheckpointFee: Balance = 0.1 * AVT;
 }
 const ASSET_ACCOUNT_DEPOSIT: Balance = 100 * MICRO_AVT;
 
