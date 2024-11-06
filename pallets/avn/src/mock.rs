15e7f3c82f22def9ea86719f2239c0049f0a6d04
Thanos Doukoudakis <56822898+thadouk@users.noreply.github.com>
2024-11-06 15:49:20 +0800

    chore: cleanup unused parameters and other types

--- pallets/avn/src/mock.rs
+++ pallets/avn/src/mock.rs
@@ -3,12 +3,12 @@
 #![cfg(test)]
 
 use crate::{self as pallet_avn, *};
-use frame_support::{derive_impl, parameter_types, weights::Weight};
+use frame_support::{derive_impl, parameter_types};
 use frame_system::{self as system, DefaultConfig};
 use hex_literal::hex;
 use pallet_session as session;
 use sp_core::offchain::testing::{OffchainState, PendingRequest};
-use sp_runtime::{testing::UintAuthorityId, traits::ConvertInto, BuildStorage, Perbill};
+use sp_runtime::{testing::UintAuthorityId, traits::ConvertInto, BuildStorage};
 use sp_state_machine::BasicExternalities;
 use std::cell::RefCell;
 
@@ -16,6 +16,10 @@ pub type AccountId = <TestRuntime as system::Config>::AccountId;
 pub type AVN = Pallet<TestRuntime>;
 
 type Block = frame_system::mocking::MockBlock<TestRuntime>;
+
+pub(crate) type SessionIndex = u32;
+pub(crate) static CUSTOM_BRIDGE_CONTRACT: H160 =
+    H160(hex!("11111AAAAA22222BBBBB11111AAAAA22222BBBBB"));
 
 frame_support::construct_runtime!(
     pub enum TestRuntime
@@ -29,47 +33,15 @@ frame_support::construct_runtime!(
 #[derive_impl(pallet_avn::config_preludes::TestDefaultConfig as pallet_avn::DefaultConfig)]
 impl Config for TestRuntime {}
 
-parameter_types! {
-    pub const BlockHashCount: u64 = 250;
-    pub const MaximumBlockWeight: Weight = Weight::from_parts(1024 as u64, 0);
-    pub const MaximumBlockLength: u32 = 2 * 1024;
-    pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
-    pub const ChallengePeriod: u64 = 2;
-}
-
 #[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
 impl system::Config for TestRuntime {
-    type RuntimeOrigin = RuntimeOrigin;
     type Nonce = u64;
-    type RuntimeCall = RuntimeCall;
     type Block = Block;
-    type RuntimeEvent = RuntimeEvent;
-    type BlockHashCount = BlockHashCount;
-    type PalletInfo = PalletInfo;
 }
 
 parameter_types! {
     pub const Period: u64 = 1;
     pub const Offset: u64 = 0;
-    pub const DisabledValidatorsThreshold: Perbill = Perbill::from_percent(33);
-}
-
-thread_local! {
-    // validator accounts (aka public addresses, public keys-ish)
-    pub static VALIDATORS: RefCell<Option<Vec<u64>>> = RefCell::new(Some(vec![1, 2, 3]));
-}
-
-pub type SessionIndex = u32;
-
-pub static CUSTOM_BRIDGE_CONTRACT: H160 = H160(hex!("11111AAAAA22222BBBBB11111AAAAA22222BBBBB"));
-
-pub struct TestSessionManager;
-impl session::SessionManager<u64> for TestSessionManager {
-    fn new_session(_new_index: SessionIndex) -> Option<Vec<u64>> {
-        VALIDATORS.with(|l| l.borrow_mut().take())
-    }
-    fn end_session(_: SessionIndex) {}
-    fn start_session(_: SessionIndex) {}
 }
 
 impl session::Config for TestRuntime {
@@ -82,6 +54,21 @@ impl session::Config for TestRuntime {
     type ValidatorIdOf = ConvertInto;
     type NextSessionRotation = session::PeriodicSessions<Period, Offset>;
     type WeightInfo = ();
+}
+
+thread_local! {
+    // validator accounts (aka public addresses, public keys-ish)
+    pub static VALIDATORS: RefCell<Option<Vec<u64>>> = RefCell::new(Some(vec![1, 2, 3]));
+}
+
+pub struct TestSessionManager;
+
+impl session::SessionManager<u64> for TestSessionManager {
+    fn new_session(_new_index: SessionIndex) -> Option<Vec<u64>> {
+        VALIDATORS.with(|l| l.borrow_mut().take())
+    }
+    fn end_session(_: SessionIndex) {}
+    fn start_session(_: SessionIndex) {}
 }
 
 pub struct ExtBuilder {
