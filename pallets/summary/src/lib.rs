2c218af8fd79a79676e3af6f6faba179b6d32ddd
ivan-cholakov <ivan.cholakov@aventus.io>
2025-05-14 12:26:03 +0300

    fixup! fix: added a function to be consummed by watchtower-runtime-api

--- pallets/summary/src/lib.rs
+++ pallets/summary/src/lib.rs
@@ -1475,8 +1475,8 @@ pub mod pallet {
             [PALLET_ID.to_vec(), vec![T::InstanceId::get()]].concat()
         }
 
-        pub(crate) fn get_summary_details_impl(
-            root_id: &RootId<BlockNumberFor<Self>>
+        pub fn get_summary_details_impl(
+            root_id: &RootId<BlockNumberFor<T>>
         ) -> Option<(H256, SummaryStatus)> {
             if <Roots<T, I>>::contains_key(root_id.range, root_id.ingress_counter) {
                 let root_data = <Roots<T, I>>::get(root_id.range, root_id.ingress_counter);
