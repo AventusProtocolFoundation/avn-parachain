4d3fd0d3d435e5e2c69cdb02a849f3c45000e0a8
Thanos <56822898+thadouk@users.noreply.github.com>
2025-06-25 22:42:32 +0100

    feat: Makes lower proof generation eip-712 compatible
    Updates the lower proof generation to be compatible with eip-712 hashing
    schema
    
    Jira ticket:
    - SYS-4560

--- primitives/avn-common/src/eth/mod.rs
+++ primitives/avn-common/src/eth/mod.rs
@@ -7,7 +7,7 @@ use alloc::string::String;
 #[cfg(not(feature = "std"))]
 use alloc::{string::String, vec::Vec};
 
-use alloy_primitives::{Address, Bytes, FixedBytes, B256};
+use alloy_primitives::{Address, Bytes, FixedBytes, B256 as AlloyB256, U256 as AlloyU256};
 use alloy_sol_types::{eip712_domain, sol, Eip712Domain, SolStruct};
 use codec::{Decode, Encode, MaxEncodedLen};
 use core::str;
@@ -16,6 +16,9 @@ use sp_io::hashing::blake2_256;
 use sp_runtime::{scale_info::TypeInfo, BoundedVec};
 use sp_std::vec::Vec;
 pub type EthereumId = u32;
+
+pub const PACKED_LOWER_PARAM_SIZE: usize = 76;
+pub type LowerParams = [u8; PACKED_LOWER_PARAM_SIZE];
 
 #[derive(Encode, Decode, Default, Clone, Debug, PartialEq, Eq, TypeInfo, MaxEncodedLen)]
 pub enum EthereumNetwork {
@@ -93,6 +96,37 @@ pub enum BridgeContractMethod {
     RemoveAuthor,
 }
 
+impl BridgeContractMethod {
+    pub fn name_as_bytes(&self) -> &[u8] {
+        match self {
+            BridgeContractMethod::ReferenceRateUpdatedAt => b"referenceRateUpdatedAt",
+            BridgeContractMethod::CheckReferenceRate => b"checkReferenceRate",
+            BridgeContractMethod::UpdateReferenceRate => b"updateReferenceRate",
+            BridgeContractMethod::PublishRoot => b"publishRoot",
+            BridgeContractMethod::TriggerGrowth => b"triggerGrowth",
+            BridgeContractMethod::AddAuthor => b"addAuthor",
+            BridgeContractMethod::RemoveAuthor => b"removeAuthor",
+        }
+    }
+}
+
+impl TryFrom<&[u8]> for BridgeContractMethod {
+    type Error = ();
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        match value {
+            b"referenceRateUpdatedAt" => Ok(BridgeContractMethod::ReferenceRateUpdatedAt),
+            b"checkReferenceRate" => Ok(BridgeContractMethod::CheckReferenceRate),
+            b"updateReferenceRate" => Ok(BridgeContractMethod::UpdateReferenceRate),
+            b"publishRoot" => Ok(BridgeContractMethod::PublishRoot),
+            b"triggerGrowth" => Ok(BridgeContractMethod::TriggerGrowth),
+            b"addAuthor" => Ok(BridgeContractMethod::AddAuthor),
+            b"removeAuthor" => Ok(BridgeContractMethod::RemoveAuthor),
+            _ => Err(()),
+        }
+    }
+}
+
 sol! {
     struct PublishRoot {
         bytes32 root;
@@ -129,39 +163,46 @@ sol! {
     }
 }
 
-impl BridgeContractMethod {
-    pub fn name_as_bytes(&self) -> &[u8] {
-        match self {
-            BridgeContractMethod::ReferenceRateUpdatedAt => b"referenceRateUpdatedAt",
-            BridgeContractMethod::CheckReferenceRate => b"checkReferenceRate",
-            BridgeContractMethod::UpdateReferenceRate => b"updateReferenceRate",
-            BridgeContractMethod::PublishRoot => b"publishRoot",
-            BridgeContractMethod::TriggerGrowth => b"triggerGrowth",
-            BridgeContractMethod::AddAuthor => b"addAuthor",
-            BridgeContractMethod::RemoveAuthor => b"removeAuthor",
-        }
+sol! {
+    // Arguments should match 'LowerProof(address token,uint256 amount,address recipient,uint32 lowerId)')
+    // But since the caller function is ClaimLower, we use that name in the struct
+    struct ClaimLower {
+        address token;
+        uint256 amount;
+        address recipient;
+        uint32 lowerId;
     }
 }
 
-impl TryFrom<&[u8]> for BridgeContractMethod {
+impl TryFrom<LowerParams> for ClaimLower {
     type Error = ();
 
-    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
-        match value {
-            b"referenceRateUpdatedAt" => Ok(BridgeContractMethod::ReferenceRateUpdatedAt),
-            b"checkReferenceRate" => Ok(BridgeContractMethod::CheckReferenceRate),
-            b"updateReferenceRate" => Ok(BridgeContractMethod::UpdateReferenceRate),
-            b"publishRoot" => Ok(BridgeContractMethod::PublishRoot),
-            b"triggerGrowth" => Ok(BridgeContractMethod::TriggerGrowth),
-            b"addAuthor" => Ok(BridgeContractMethod::AddAuthor),
-            b"removeAuthor" => Ok(BridgeContractMethod::RemoveAuthor),
-            _ => Err(()),
+    fn try_from(lower_params: LowerParams) -> Result<Self, Self::Error> {
+        if lower_params.len() != PACKED_LOWER_PARAM_SIZE {
+            return Err(())
         }
+
+        let token = Address::from_slice(&lower_params[0..20]);
+        let amount = AlloyU256::try_from_be_slice(&lower_params[36..52]).ok_or(())?;
+        let recipient = Address::from_slice(&lower_params[52..72]);
+        let lower_id = u32::from_be_bytes(lower_params[72..76].try_into().map_err(|_| ())?);
+
+        Ok(ClaimLower { token, amount, recipient, lowerId: lower_id })
     }
+}
+
+pub fn create_active_lower_hash(
+    lower_params: LowerParams,
+    domain: Eip712Domain,
+) -> Result<H256, ()> {
+    let claim_lower = ClaimLower::try_from(lower_params)?;
+    let hash = eip712_hash(&claim_lower, &domain);
+
+    Ok(hash)
 }
 
 pub fn eip712_hash<T: SolStruct>(data: &T, domain: &Eip712Domain) -> H256 {
-    let hash: B256 = data.eip712_signing_hash(domain);
+    let hash: AlloyB256 = data.eip712_signing_hash(domain);
     H256::from_slice(&hash.0)
 }
 
@@ -219,7 +260,7 @@ pub fn create_function_confirmation_hash(
             }
 
             let t1_pub_key: Bytes = Bytes::from(params[0].1.clone());
-            let t2_pub_key: B256 = B256::from_slice(&params[1].1);
+            let t2_pub_key: AlloyB256 = AlloyB256::from_slice(&params[1].1);
 
             let (tx_id, expiry) = extract_tx_id_and_expiry(&params)?;
             let data = AddAuthor { t1_pub_key, t2_pub_key, expiry, tx_id };
@@ -231,7 +272,7 @@ pub fn create_function_confirmation_hash(
                 return Err(())
             }
 
-            let t2_pub_key: B256 = B256::from_slice(&params[0].1);
+            let t2_pub_key: AlloyB256 = AlloyB256::from_slice(&params[0].1);
             let t1_pub_key: Bytes = Bytes::from(params[1].1.clone());
 
             let (tx_id, expiry) = extract_tx_id_and_expiry(&params)?;
