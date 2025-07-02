17442da95f7ee7cdb1fc63b0da24f72dd612ec29
Thanos <56822898+thadouk@users.noreply.github.com>
2025-07-02 18:12:47 +0100

    fix: Signing context of unsigned operations
    Aligns validation and hashing to use the instance hash.

--- pallets/eth-bridge/src/call.rs
+++ pallets/eth-bridge/src/call.rs
@@ -67,7 +67,8 @@ fn add_confirmation_proof<T: Config<I>, I: 'static>(
     confirmation: &ecdsa::Signature,
     account_id: &T::AccountId,
 ) -> Vec<u8> {
-    (Instance::<T, I>::get(), ADD_CONFIRMATION_CONTEXT, tx_id, confirmation, &account_id).encode()
+    (Instance::<T, I>::get().hash(), ADD_CONFIRMATION_CONTEXT, tx_id, confirmation, &account_id)
+        .encode()
 }
 
 fn add_eth_tx_hash_proof<T: Config<I>, I: 'static>(
@@ -75,7 +76,8 @@ fn add_eth_tx_hash_proof<T: Config<I>, I: 'static>(
     eth_tx_hash: &H256,
     account_id: &T::AccountId,
 ) -> Vec<u8> {
-    (Instance::<T, I>::get(), ADD_ETH_TX_HASH_CONTEXT, tx_id, *eth_tx_hash, &account_id).encode()
+    (Instance::<T, I>::get().hash(), ADD_ETH_TX_HASH_CONTEXT, tx_id, *eth_tx_hash, &account_id)
+        .encode()
 }
 
 fn add_corroboration_proof<T: Config<I>, I: 'static>(
@@ -85,7 +87,7 @@ fn add_corroboration_proof<T: Config<I>, I: 'static>(
     account_id: &T::AccountId,
 ) -> Vec<u8> {
     (
-        Instance::<T, I>::get(),
+        Instance::<T, I>::get().hash(),
         ADD_CORROBORATION_CONTEXT,
         tx_id,
         tx_succeeded,
