e78487b5edf0cf32c347717307ba88eaa7b685e1
MIcael Franco <micael.franco@aventus.io>
2025-08-22 13:48:18 +0100

    Pallet setup

---
+++ pallets/avn-oracle/src/lib.rs
@@ -0,0 +1,80 @@
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
+
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        /// The overarching runtime event type.
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+        /// A type representing the weights required by the dispatchables of this pallet.
+        type WeightInfo;
+    }
+
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        /// A user has successfully set a new value.
+        SomethingStored {
+            /// The new value set.
+            something: u32,
+            /// The account who set the new value.
+            who: T::AccountId,
+        },
+    }
+
+    #[pallet::error]
+    pub enum Error<T> {
+        /// The value retrieved was `None` as no value was previously set.
+        NoneValue,
+        /// There was an attempt to increment the value in storage over `u32::MAX`.
+        StorageOverflow,
+    }
+
+    #[pallet::storage]
+    pub type Something<T> = StorageValue<_, u32>;
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::call_index(0)]
+        #[pallet::weight(Weight::default())]
+        pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
+            // Check that the extrinsic was signed and get the signer.
+            let who = ensure_signed(origin)?;
+
+            // Update storage.
+            Something::<T>::put(something);
+
+            // Emit an event.
+            Self::deposit_event(Event::SomethingStored { something, who });
+
+            // Return a successful `DispatchResult`
+            Ok(())
+        }
+
+        #[pallet::call_index(1)]
+        #[pallet::weight(Weight::default())]
+        pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
+            let _who = ensure_signed(origin)?;
+
+            // Read a value from storage.
+            match Something::<T>::get() {
+                // Return an error if the value has not been set.
+                None => Err(Error::<T>::NoneValue.into()),
+                Some(old) => {
+                    // Increment the value read from storage. This will cause an error in the event
+                    // of overflow.
+                    let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+                    // Update the value in storage with the incremented result.
+                    Something::<T>::put(new);
+                    Ok(())
+                },
+            }
+        }
+    }
+}
--- runtime/avn/src/lib.rs
+++ runtime/avn/src/lib.rs
@@ -662,6 +662,11 @@ impl pallet_avn_anchor::Config for Runtime {
     type DefaultCheckpointFee = DefaultCheckpointFee;
 }
 
+impl pallet_avn_oracle::Config for Runtime {
+    type RuntimeEvent = RuntimeEvent;
+    type WeightInfo = ();
+}
+
 use sp_avn_common::{event_discovery::EthBridgeEventsFilter, event_types::ValidEvents};
 
 impl pallet_eth_bridge::Config for Runtime {
@@ -819,6 +824,7 @@ construct_runtime!(
         AvnTransactionPayment: pallet_avn_transaction_payment = 90,
         EthBridge: pallet_eth_bridge = 91,
         AvnAnchor: pallet_avn_anchor = 92,
+        AvnOracle: pallet_avn_oracle = 93,
 
         // OpenGov pallets
         Preimage: pallet_preimage::{Pallet, Call, Storage, Event<T>, HoldReason} = 97,
