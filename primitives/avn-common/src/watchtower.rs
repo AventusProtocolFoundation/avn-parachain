192e6072de2f612e7c89f1c88de1708164bc956f
nahuseyoum <nahu.seyoum@aventus.io>
2025-09-24 13:58:32 +0100

    Adds decision rule back in

--- primitives/avn-common/src/watchtower.rs
+++ primitives/avn-common/src/watchtower.rs
@@ -52,12 +52,19 @@ impl Default for ProposalStatusEnum {
     }
 }
 
+#[derive(Encode, Decode, RuntimeDebug, Clone, PartialEq, Eq, TypeInfo, MaxEncodedLen)]
+pub enum DecisionRule {
+    /// Yes > No to win
+    SimpleMajority,
+}
+
 #[derive(Encode, Decode, RuntimeDebug, Clone, PartialEq, Eq, TypeInfo)]
 pub struct ProposalRequest {
     pub title: Vec<u8>,
     pub payload: RawPayload,
     pub threshold: Perbill,
     pub source: ProposalSource,
+    pub decision_rule: DecisionRule,
     /// A unique ref provided by the proposer. Used when sending notifications about this proposal.
     pub external_ref: H256,
     pub created_at: u32,
