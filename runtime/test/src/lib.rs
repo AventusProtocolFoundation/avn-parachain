2a898ec337218e6b6238627b5c5d87b122eafb39
Thanos <56822898+thadouk@users.noreply.github.com>
2025-10-22 11:04:32 +0100

    chore: increase spec_version of runtime

--- runtime/test/src/lib.rs
+++ runtime/test/src/lib.rs
@@ -180,7 +180,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("avn-test-parachain"),
     impl_name: create_runtime_str!("avn-test-parachain"),
     authoring_version: 1,
-    spec_version: 117,
+    spec_version: 118,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
