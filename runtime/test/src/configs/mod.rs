d95214bff81b3452221f296de57b3ab0151ea5de
Thanos <56822898+thadouk@users.noreply.github.com>
2025-11-27 11:18:39 +0000

    chore: upgrade Polkadot SDK to v1.13.0
    - Updates all crates to Polkadot SDK v1.13.0 and adapts code to API/interface changes
    
    Jira:
    - SYS-4677
    - SYS-4551

--- runtime/test/src/configs/mod.rs
+++ runtime/test/src/configs/mod.rs
@@ -217,7 +217,7 @@ impl pallet_message_queue::Config for Runtime {
     // The XCMP queue pallet is only ever able to handle the `Sibling(ParaId)` origin:
     type QueueChangeHandler = NarrowOriginToSibling<XcmpQueue>;
     type QueuePausedQuery = NarrowOriginToSibling<XcmpQueue>;
-    type HeapSize = sp_core::ConstU32<{ 64 * 1024 }>;
+    type HeapSize = sp_core::ConstU32<{ 103 * 1024 }>;
     type MaxStale = sp_core::ConstU32<8>;
     type ServiceWeight = MessageQueueServiceWeight;
     // TODO 1.10 review this
@@ -233,6 +233,8 @@ impl cumulus_pallet_xcmp_queue::Config for Runtime {
     // Enqueue XCMP messages from siblings for later processing.
     type XcmpQueue = TransformOrigin<MessageQueue, AggregateMessageOrigin, ParaId, ParaIdToSibling>;
     type MaxInboundSuspended = sp_core::ConstU32<1_000>;
+    type MaxActiveOutboundChannels = ConstU32<128>;
+    type MaxPageSize = ConstU32<{ 1 << 16 }>;
     type ControllerOrigin = EnsureRoot<AccountId>;
     type ControllerOriginConverter = XcmOriginToTransactDispatchOrigin;
     type WeightInfo = ();
